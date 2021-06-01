20b512ece105a6adbd34d54f0c00269fd536aed6
Jane <41706751+Zeexel@users.noreply.github.com>
2021-06-01 13:10:47 -0700

    Add a check for outdated configs

--- source/STOptionsRewrite.hx
+++ source/STOptionsRewrite.hx
@@ -12,6 +12,7 @@ import sys.io.File;
 
 // variables type
 typedef Variables = {
+    var STVersion:String;
     var customIntro:Bool;
     var disableFNFVersionCheck:Bool;
     var debug:Bool;
@@ -63,6 +64,7 @@ class STOptionsRewrite
         if (!FileSystem.exists('config.json'))
         {
             _variables = {
+                STVersion: "3.1.0",
                 customIntro: true,
                 debug: false,
                 disableFNFVersionCheck: true,
@@ -103,5 +105,66 @@ class STOptionsRewrite
             var data:String = File.getContent('config.json');
             _variables = Json.parse(data);
         }
+        // Regenerate config when we're out of date.
+        // This WILL reset everything to default values.
+        if (checkSTVersion() == false)
+        {
+            _variables = {
+                STVersion: "3.1.0",
+                customIntro: true,
+                debug: false,
+                disableFNFVersionCheck: true,
+                discordRPC: true,
+                extraDialogue: true,
+                extraSongs: true,
+                fixMonsterIconFreeplay: true,
+                fixScoreLayout: true,
+                fixWeek6CountSounds: true,
+                hideOptionsMenu: false,
+                instMode: false,
+                logNG: true,
+                lyrics: true,
+                makeSpacesConsistent: true,
+                monsterIntro: true,
+                noticeEnabled: false,
+                outlinePauseInfo: true,
+                outlineScore: true,
+                songIndicator: true,
+                startWHP2Invis: true,
+                unknownIcons: true,
+                updatedInputSystem: true,
+                missCounter: true,
+                accuracyMeter: true,
+                downscroll: false,
+                cameraMovement: true,
+                backgroundMovement: true,
+                upBind: "W",
+                downBind: "S",
+                leftBind: "A",
+                rightBind: "D"
+            };
+
+            Save();
+
+            trace("Reset config.json");
+        }
+    }
+
+    // Check for an outdated config
+    // Returns true if the version is up to date
+    // Returns false if there is no version or if we're out of date
+    public static function checkSTVersion()
+    {  
+        // Don't ask why I check it up against the MainMenuState, I was out of ideas.
+        if (_variables.STVersion != MainMenuState.smallThingsVersion
+            || _variables.STVersion == null) {
+                trace(_variables.STVersion);
+                trace("Config.json out of date! Regenerating..");
+                return false;
+            } else {
+                trace(_variables.STVersion);
+                trace("Config.json is up to date!");
+                return true;
+            }
     }
 }
